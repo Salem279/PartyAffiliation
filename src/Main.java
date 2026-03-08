4a3d31936448b6a754c31ad2a2e4ec5d075d55b6
Ammar Salem <salema2@mail.uc.edu>
2026-03-07 21:01:12 -0500

    a

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        String partyChoice = "";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Choose your party affiliation:");
+        System.out.println("D - Democrat\nR - Republican\nI - Independent");
+        System.out.print("Enter your choice [D, R, I]: ");
+
+        if (in.hasNextLine()) {
+            partyChoice = in.nextLine();
+
+            // Cascaded if-else structure
+            if (partyChoice.equalsIgnoreCase("D")) {
+                System.out.println("You get a Democratic Donkey.");
+            } else if (partyChoice.equalsIgnoreCase("R")) {
+                System.out.println("You get a Republican Elephant.");
+            } else if (partyChoice.equalsIgnoreCase("I")) {
+                System.out.println("You get an Independent Person.");
+            } else {
+                System.out.println("You get an Other.");
+            }
         }
     }
 }
